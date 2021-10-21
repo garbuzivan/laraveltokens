5e60123ac14f90219face7e52e414ae3be869767
Гарбуз Иван <i.garbuz@zebrains.team>
2021-10-21 15:18:38 +0300

    Добавление трейтов для создания и удаления токенов пользователя для модели пользователя

--- src/Traits/UserTrait.php
+++ src/Traits/UserTrait.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Garbuzivan\Laraveltokens\Traits;
 
+use DateTime;
+use Garbuzivan\Laraveltokens\Exceptions\UserNotExistsException;
 use Garbuzivan\Laraveltokens\Models\Token;
+use Garbuzivan\Laraveltokens\TokenManager;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 trait UserTrait
@@ -15,5 +18,26 @@ trait UserTrait
     public function tokens()
     {
         return $this->/** @scrutinizer ignore-call */ hasMany(Token::class);
+    }
+
+    /**
+     * Создать новый токен пользователя
+     * @param string $title
+     * @param DateTime|null $expiration
+     * @return Token
+     * @throws UserNotExistsException
+     */
+    public function tokenCreate(string $title = '', ?DateTime $expiration = null): Token
+    {
+        return app(TokenManager::class)->create($title, $expiration, $this->id);
+    }
+
+    /**
+     * Удалить все токены пользователя
+     * @return bool
+     */
+    public function tokensDelete(): bool
+    {
+        return app(TokenManager::class)->deleteByUser($this->id);
     }
 }
